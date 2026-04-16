64450b7359f51980596d438af3b5800245ec1e06
Agustin <aguspinto08@gmail.com>
2026-04-16 16:11:54 -0300

    1234

--- Ej12.cs
+++ Ej12.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Formats.Asn1;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography.X509Certificates;
 
 /*Hacer una clase llamada Persona que siga las siguientes condiciones:
@@ -17,16 +18,16 @@ constante para ello.
 ○ Un constructor con el nombre, Edad y Sexo, el resto por defecto.
 ○ Un constructor con todos los atributos como parámetro.
 ● Los métodos que se implementarán son:
-○ calcularIMC(): calculará si la persona está en su peso ideal (peso en
+○ CalcularIMC(): calculará si la persona está en su peso ideal (peso en
 kg/(altura^2 en m)), si esta fórmula devuelve un valor menor que 20, la función
 devuelve un -1, si devuelve un número entre 20 y 25 (incluidos), significa que
 está por debajo de su peso ideal la función devuelve un 0 y si devuelve un valor
 mayor que 25 significa que tiene sobrepeso, la función devuelve un 1. Te
 recomiendo que uses constantes para devolver estos valores.
-■ esMayorDeEdad(): indica si es mayor de Edad, devuelve un booleano.
-■ comprobarSexo(char Sexo): comprueba que el Sexo introducido es
+■ EsMayorDeEdad(): indica si es mayor de Edad, devuelve un booleano.
+■ ComprobarSexo(char Sexo): comprueba que el Sexo introducido es
 correcto. Si no es correcto, será H. No será visible al exterior.
-■ generaDni(): genera un número aleatorio de 8 cifras, genera a partir de
+■ GenerarDNI(): genera un número aleatorio de 8 cifras, genera a partir de
 este su número su letra correspondiente. Este método será invocado
 cuando se construya el objeto. Podes dividir el método para que te sea
 más fácil. No será visible al exterior ejemplo de Dni(12345678D).
@@ -48,7 +49,7 @@ namespace Test{
             
             try{
                 Console.Write("Nombre: ");
-                string? nombre = Console.ReadLine();
+                string nombre = Console.ReadLine();
                 Console.Write("Edad: ");
                 byte edad = byte.Parse(Console.ReadLine());
                 Console.Write("Sexo: ");
@@ -74,7 +75,7 @@ namespace Test{
 //nombre, Edad, Dni, Sexo (H hombre, M mujer), peso y altura
     public class Persona{
         private const char SEXO_DEFAULT = 'H';
-        private string? Nombre;
+        private string Nombre;
         private byte Edad;
         private string Dni;
         private char Sexo;
@@ -101,14 +102,14 @@ namespace Test{
 //○ Un constructor por defecto.
         public Persona(){
             this.Sexo = SEXO_DEFAULT;
-            this.Dni = generaDni();
+            this.Dni = GenerarDNI();
         }
 //○ Un constructor con el nombre, Edad y Sexo, el resto por defecto.
         public Persona(string nombre, byte Edad, char Sexo){
             this.Nombre = nombre;
             this.Edad = Edad;
             this.Sexo = Sexo;
-            this.Dni = generaDni();
+            this.Dni = GenerarDNI();
         }
 //○ Un constructor con todos los atributos como parámetro.
         public Persona(string nombre, byte edad, char sexo, int dni, decimal peso, decimal altura){
@@ -123,13 +124,13 @@ namespace Test{
             this.Nombre = nombre;
             this.Edad = edad;
             this.Sexo = sexo;
-            this.Dni = generaDni();
+            this.Dni = GenerarDNI();
             this.Peso = peso;
             this.altura = altura;
         }
         public void Mostrar(){
             //string PesoIdeal(int pesoIdeal) => pesoIdeal == -1 ? $"{Nombre} esta en su peso ideal" : pesoIdeal == 0 ? $"{Nombre} esta por debajo de su peso ideal" : $"{Nombre} tiene sobrepeso";
-            string PesoIdeal = calcularIMC() switch{
+            string PesoIdeal = CalcularIMC() switch{
                 -1 => $"\n{Nombre} esta en su peso ideal",
                 0 => $"\n{Nombre} esta por debajo de su peso ideal",
                 1 => $"\n{Nombre} tiene sobrepeso",
@@ -147,48 +148,39 @@ namespace Test{
             Console.WriteLine($"{PesoIdeal}");
             Console.WriteLine("------------------------------\n");
         }
-/*calcularIMC(): calculará si la persona está en su peso ideal (peso en
+/*CalcularIMC(): calculará si la persona está en su peso ideal (peso en
 kg/(altura^2 en m)), si esta fórmula devuelve un valor menor que 20, la función
 devuelve un -1, si devuelve un número entre 20 y 25 (incluidos), significa que
 está por debajo de su peso ideal la función devuelve un 0 y si devuelve un valor
 mayor que 25 significa que tiene sobrepeso, la función devuelve un 1. Te
 recomiendo que uses constantes para devolver estos valores.*/
-        public int calcularIMC(){
+        public int CalcularIMC(){
             int EsIdeal = 2;
             if(peso > 0 && altura > 0){
-                decimal result = peso / (altura*altura);
-                switch(result){
-                    case < 20:
-                        EsIdeal = -1;
-                        break;
-                    case >= 20 and <= 25:
-                        EsIdeal = 0;
-                        break;
-                    default:
-                        EsIdeal = 1;
-                        break;
+                switch(peso / (altura*altura)){
+                    case < 20: EsIdeal = -1; break;
+                    case >= 20 and <= 25: EsIdeal = 0; break;
+                    default: EsIdeal = 1; break;
                 }
             }
             return EsIdeal;
         }
-//■ esMayorDeEdad(): indica si es mayor de Edad, devuelve un booleano.
-        public bool esMayorDeEdad(){
-            bool esMayor = false;
-            if(Edad >= 18)
-                esMayor = true;
-            return esMayor;
+//■ EsMayorDeEdad(): indica si es mayor de Edad, devuelve un booleano.
+        public bool EsMayorDeEdad(){
+            bool EsMayor(int edad) => edad >= 18 ? true : false;
+            return EsMayor(Edad);
         }
-/*■ comprobarSexo(char Sexo): comprueba que el Sexo introducido es
+/*■ ComprobarSexo(char Sexo): comprueba que el Sexo introducido es
 correcto. Si no es correcto, será H. No será visible al exterior.*/
-        public void comprobarSexo(){
+        public void ComprobarSexo(){
             if(!(Sexo == 'M' || Sexo == 'H'))
                 Sexo = 'H';
         }
-/*■ generaDni(): genera un número aleatorio de 8 cifras, genera a partir de
+/*■ GenerarDNI(): genera un número aleatorio de 8 cifras, genera a partir de
 este su número su letra correspondiente. Este método será invocado
 cuando se construya el objeto. Podes dividir el método para que te sea
 más fácil. No será visible al exterior ejemplo de Dni(12345678D).*/
-        static public string generaDni(){
+        static public string GenerarDNI(){
             int rnd_Dni = new Random().Next(10000000,99999999);
             return $"{DniConLetra(rnd_Dni)}";
         }
